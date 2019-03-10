270fca10db17e9672e4cd355212acde68312c9a2
Esteban <eechava6@eafit.edu.co>
2019-03-10 14:11:52 -0500

    MPI script hot fix

--- mpiMM.sh
+++ mpiMM.sh
@@ -10,26 +10,26 @@ np=$1
 echo "\n MPI 1k"
 for i in 1 2 3
 do
-  mpirun -$np -f hosts_mpi  ./mmMPI 1000
+  mpirun -np $np -f hosts_mpi  ./mmMPI 1000
 done
 
 
 echo "\n MPI 4k"
 for i in 1 2 3 
 do
-   mpirun -$np -f hosts_mpi  ./mmMPI 4000
+   mpirun -np $np -f hosts_mpi  ./mmMPI 4000
 done
 
 echo "\n MPI 5k"
 for i in 1 2 3 
 do
-   mpirun -$np -f hosts_mpi  ./mmMPI 5000
+   mpirun -np $np -f hosts_mpi  ./mmMPI 5000
 done
 
 }
 
 
-for h in 2 3 4 8 12 16 32 48 64 70
+for h in 2 
 do
   echo "NP $h"
   iterations $h
