d0ee62380bb1f017176930be75265065b5512d49
Esteban <eechava6@eafit.edu.co>
2019-03-10 14:14:17 -0500

    MPI SCRIPT COMPLETED

--- mpiMM.sh
+++ mpiMM.sh
@@ -7,20 +7,20 @@ iterations()
 {
 np=$1
 
-echo "\n MPI 1k"
+echo -e  "\n MPI 1k"
 for i in 1 2 3
 do
   mpirun -np $np -f hosts_mpi  ./mmMPI 1000
 done
 
 
-echo "\n MPI 4k"
+echo -e  "\n MPI 4k"
 for i in 1 2 3 
 do
    mpirun -np $np -f hosts_mpi  ./mmMPI 4000
 done
 
-echo "\n MPI 5k"
+echo -e  "\n MPI 5k"
 for i in 1 2 3 
 do
    mpirun -np $np -f hosts_mpi  ./mmMPI 5000
@@ -29,11 +29,11 @@ done
 }
 
 
-for h in 2 
+for h in 2 3 4 8 12 16 32 48 64 70
 do
-  echo "NP $h"
+  echo -e  "NP $h"
   iterations $h
-  echo "_______________________________\n"
+  echo -e  "_______________________________\n"
 
 done
 
