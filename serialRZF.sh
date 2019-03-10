91f6107aebc0f5941098da7a275d52816c598b46
Esteban <eechava6@eafit.edu.co>
2019-03-10 10:43:48 -0500

    Improved folder management, added RFZ serial 1000k

--- serialRZF.sh
+++ serialRZF.sh
@@ -22,12 +22,21 @@ done
 for i in 1 2 3 4 5
 do
 
-  time ./rzfserial 10000000
-  echo "Serial 10m iteration: $i"
-   >&2 echo "Serial 10m iteration: $i"
+  time ./rzfserial 100000000
+  echo "Serial 100m iteration: $i"
+   >&2 echo "Serial 100m iteration: $i"
 done
 >&2 echo "_____________________________"
 
+
+for i in 1 2 3 4 5
+do
+
+  time ./rzfserial 1000000000
+  echo "Serial 1000m iteration: $i"
+   >&2 echo "Serial 1000m iteration: $i"
+done
+>&2 echo "_____________________________"
 
 
 
